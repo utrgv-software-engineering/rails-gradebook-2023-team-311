d8f60a5ec9c723d60ce626d477864589817d8196
Brandon Rubio <brandon.rubio01@utrgv.edu>
2023-05-10 19:42:42 +0000

    Ramdom student Id generates

--- spec/factories/grades.rb
+++ spec/factories/grades.rb
@@ -1,7 +1,10 @@
+require 'securerandom'
+
 FactoryBot.define do
   factory :grade do
-    sequence(:student_id){ |n| "#{n}#{n}#{n}#{n}#{n}"}
+    sequence(:student_id) { SecureRandom.hex(3) } # generates a random hexadecimal string of 6 characters
     sequence(:student_name) { |n| "Student #{n}" }
+    student_grade { rand(1..100) } # assign a random grade from 1 to 100
 
     trait :valid do
       student_grade { 70 }
