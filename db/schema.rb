4507703cf31188bd8b77ab8e743ad5b93395e6bf
Ethen Sanchez <ethen.sanchez01@utrgv.edu>
2023-05-11 23:05:18 +0000

    Updates permissions and testing for FPC(11,12,14,17)

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_034901) do
     t.datetime "remember_created_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role_id"
+    t.integer "role_id", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
