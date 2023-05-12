5857349219ff02a96cb5f7541f6e4c06bbff5274
Ethen Sanchez <ethen.sanchez01@utrgv.edu>
2023-05-12 04:06:59 +0000

    Changed master key

--- db/migrate/20230502174924_add_account_id_to_users.rb
+++ db/migrate/20230502174924_add_account_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddAccountIdToUsers < ActiveRecord::Migration[7.0]
         def change
-          add_column :users, :role_id, :integer
+          add_column :users, :role_id, :integer, default: 1
         end
       end
