3097ba1a9994bb9b29bc29672379f95945669eff
ethen-sanchez <109364835+ethen-sanchez@users.noreply.github.com>
2023-05-11 22:23:53 -0500

    Updated for rspec testing
    Defaulting to nil was causing rspec testing to fail. Defaulted 1 to test teacher account.

--- db/migrate/20230502174924_add_account_id_to_users.rb
+++ db/migrate/20230502174924_add_account_id_to_users.rb
@@ -1,5 +1,6 @@
 class AddAccountIdToUsers < ActiveRecord::Migration[7.0]
         def change
-          add_column :users, :role_id, :integer
+          add_column :users, :role_id, :integer, default: 1
+                #added default 1 to use rspec testing.
         end
       end
